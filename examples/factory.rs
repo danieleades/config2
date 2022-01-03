da05628ca385b10e806a86e21398998b722a8a5a
Daniel Eades <danieleades@hotmail.com>
2022-01-03 09:17:32 +0000

    tidy examples

--- examples/factory.rs
+++ examples/factory.rs
@@ -1,44 +1,10 @@
-#![allow(unused)]
-
 use anyhow::Result;
-use config2::{source, Error, Layered, Partial};
-use serde::Deserialize;
+use config2::{source, Layered};
 
-#[derive(Debug)]
+#[derive(Debug, Layered)]
 struct Config {
     field_a: i32,
     field_b: String,
-}
-
-impl Layered for Config {
-    type Layer = PartialConfig;
-}
-
-#[derive(Default, Deserialize)]
-struct PartialConfig {
-    field_a: Option<i32>,
-    field_b: Option<String>,
-}
-
-impl Partial for PartialConfig {
-    type T = Config;
-
-    fn merge(&mut self, other: Self) {
-        todo!()
-    }
-
-    fn build(self) -> Result<Self::T, Error> {
-        todo!()
-    }
-}
-
-impl From<Config> for PartialConfig {
-    fn from(config: Config) -> Self {
-        Self {
-            field_a: Some(config.field_a),
-            field_b: Some(config.field_b),
-        }
-    }
 }
 
 fn config_factory() -> anyhow::Result<Config> {
