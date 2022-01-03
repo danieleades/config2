f0b26d4eca400b09a93fbfbe606ee7dbfdff3752
danieleades <33452915+danieleades@users.noreply.github.com>
2022-01-03 17:35:17 +0000

    refactor derive crate (#5)

--- config2-derive/src/lib.rs
+++ config2-derive/src/lib.rs
@@ -1,12 +1,27 @@
+//! Provides the derive macro for the `config2` crate
+
+#![deny(clippy::all, missing_debug_implementations)]
+#![warn(clippy::pedantic)]
+#![allow(clippy::missing_panics_doc)]
+
 use proc_macro::TokenStream;
 use syn::DeriveInput;
 
-mod partial;
+mod struct_data;
 
 #[proc_macro_derive(Layered)]
 pub fn derive_layered(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
-    let data = partial::Data::from(&ast);
 
-    data.build().into()
+    let name = &ast.ident;
+    let generics = &ast.generics;
+
+    match &ast.data {
+        syn::Data::Struct(data) => {
+            let fields = &data.fields;
+            let data = struct_data::Data::new(name, generics, fields);
+            data.build().into()
+        }
+        _ => panic!("'Layered' can only be derived for structs"),
+    }
 }
